3a01cacbbc04505baf7cf1491971510c61452fae
Wai-Yan-Htoo <66287776+Wai-Yan-Htoo@users.noreply.github.com>
2020-11-16 17:55:01 -1100

    Add files via upload

---
+++ AZSShopping/addtocart.Designer.cs
@@ -0,0 +1,224 @@
+﻿
+namespace AZSShopping
+{
+    partial class addtocart
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(addtocart));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt_incnd = new System.Windows.Forms.TextBox();
+            this.btn_plus = new System.Windows.Forms.Button();
+            this.btn_minus = new System.Windows.Forms.Button();
+            this.btn_order = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dg = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(284, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CAFECITO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(285, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(380, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "kfsd;fjdsfjsfsdfj;sdlfjdsvnodvnd;vdvjdsjfodsjfo;dsjfsdof\r\n";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 7);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(238, 166);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(284, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(344, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "fnlfnklfndslnvdvndlvmdmvdlvdmsvodsfsodfs;dfm\r\n";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(284, 153);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(311, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "fje;ofjeofjofjo; jljfpewofjpojfsodfjodvjd;jvodgjj\r\n";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(285, 215);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(354, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "d;gm;lgmolvjdfvmf;dlmvdf;gdrpopjgepkgepwkwe;k\r\n";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(41, 219);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(183, 24);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "EXP: 17 . Nov . 2021";
+            // 
+            // txt_incnd
+            // 
+            this.txt_incnd.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_incnd.Location = new System.Drawing.Point(71, 264);
+            this.txt_incnd.Name = "txt_incnd";
+            this.txt_incnd.Size = new System.Drawing.Size(39, 31);
+            this.txt_incnd.TabIndex = 12;
+            this.txt_incnd.Text = "1";
+            this.txt_incnd.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btn_plus
+            // 
+            this.btn_plus.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_plus.Location = new System.Drawing.Point(109, 264);
+            this.btn_plus.Name = "btn_plus";
+            this.btn_plus.Size = new System.Drawing.Size(33, 30);
+            this.btn_plus.TabIndex = 11;
+            this.btn_plus.Text = "+";
+            this.btn_plus.UseVisualStyleBackColor = true;
+            // 
+            // btn_minus
+            // 
+            this.btn_minus.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_minus.Location = new System.Drawing.Point(39, 264);
+            this.btn_minus.Name = "btn_minus";
+            this.btn_minus.Size = new System.Drawing.Size(33, 30);
+            this.btn_minus.TabIndex = 10;
+            this.btn_minus.Text = "-";
+            this.btn_minus.UseVisualStyleBackColor = true;
+            // 
+            // btn_order
+            // 
+            this.btn_order.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_order.Location = new System.Drawing.Point(177, 264);
+            this.btn_order.Name = "btn_order";
+            this.btn_order.Size = new System.Drawing.Size(121, 30);
+            this.btn_order.TabIndex = 13;
+            this.btn_order.Text = "ORDER";
+            this.btn_order.UseVisualStyleBackColor = true;
+            this.btn_order.Click += new System.EventHandler(this.btn_order_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(335, 264);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 30);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Cancel";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dg
+            // 
+            this.dg.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dg.Location = new System.Drawing.Point(484, 255);
+            this.dg.Name = "dg";
+            this.dg.Size = new System.Drawing.Size(464, 195);
+            this.dg.TabIndex = 15;
+            // 
+            // addtocart
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(960, 486);
+            this.Controls.Add(this.dg);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_order);
+            this.Controls.Add(this.txt_incnd);
+            this.Controls.Add(this.btn_plus);
+            this.Controls.Add(this.btn_minus);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "addtocart";
+            this.Text = "addtocart";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dg)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt_incnd;
+        private System.Windows.Forms.Button btn_plus;
+        private System.Windows.Forms.Button btn_minus;
+        private System.Windows.Forms.Button btn_order;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dg;
+    }
+}
---
+++ AZSShopping/addtocart.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace AZSShopping
+{
+    public partial class addtocart : Form
+    {
+        public addtocart()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 f1 = new Form1();
+            this.Hide();
+            f1.Show();
+        }
+
+        private void btn_order_Click(object sender, EventArgs e)
+        {
+            //db
+            string connectionstring = string.Format("server=localhost;uid=root;pwd=;database=office");
+            MySqlConnection conn = new MySqlConnection(connectionstring);
+            conn.Open();
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            adapter.SelectCommand = new MySqlCommand("select * from tbl_item", conn);
+            DataTable dt;
+            DataSet ds=new DataSet();
+            DataRow dr;
+
+            adapter.Fill(ds, "0"); // function(Fill( dataset , string ))
+            dt = ds.Tables["0"];  // dataset call to datatable [ string  ]
+            dr = dt.Rows[0];    //row start index 0
+            dg.DataSource = ds.Tables["0"];  //datasource get from ds to table
+
+
+
+            /*Form1 f1 = new Form1();
+            this.Hide();
+            f1.Show();*/
+        }
+    }
+}
